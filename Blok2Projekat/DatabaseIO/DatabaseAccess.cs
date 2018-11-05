673c9cab8ab0e1663be7f2bae6fcf5689aa41035
AshenKhan <39307368+AshenKhan@users.noreply.github.com>
2018-11-05 13:20:25 +0100

    Kostur Projekta

---
+++ Blok2Projekat/DatabaseIO/DatabaseAccess.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseIO
+{
+    //Format primljenih zahteva za pisanje/izmenu: SID:X;Timestamp:X;Details:X;
+    //Pri upisu, dodaje se na pocetak ID:X;
+    public class DatabaseAccess
+    {
+        /// <summary>
+        /// Konstruktor DatabaseAccess.
+        /// </summary>
+        /// <param name="fileName">Naziv .txt fajla sa kojim zelimo da radimo.</param>
+        public DatabaseAccess(string fileName)
+        {
+
+        }
+        /// <summary>
+        /// Iscitavanje zeljenog dela baze podataka.
+        /// </summary>
+        /// <param name="sid">SID podataka koje zelimo da citamo. Ako je prazan, cita se citava baza podataka.</param>
+        /// <returns>Zeljeni deo baze.</returns>
+        public string Read(string sid)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Dodavanje nove linije podataka u bazi podataka na koju je DatabaseAccess inicijalizovan. Ovde je potrebno uraditi generaciju id-a.
+        /// </summary>
+        /// <param name="data">Novi podatak koji treba upisati. Formatiranje podatka je odgovornost pozivaoca. Na kraj podatka treba dodati \n.</param>
+        /// <returns>True u slucaju pravilnog upisa, inace false.</returns>
+        public bool Write(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Izmena linije podataka u bazi podataka na koju je DatabaseAccess inicijalizovan.
+        /// </summary>
+        /// <param name="id">Identifikator reda koji treba izmeniti.</param>
+        /// <param name="data">Novi podatak koji treba upisati. Formatiranje podatka je odgovornost pozivaoca. Na kraj podatka treba dodati \n.</param>
+        /// <returns>True u slucaju pravilnog upisa, inace false.</returns>
+        public bool Edit(string id, string data)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Brisanje linije podataka u bazi podataka na koju je DatabaseAccess inicijalizovan.
+        /// </summary>
+        /// <param name="id">Identifikator reda koji treba izbrisati.</param>
+        /// <returns>True u slucaju uspesnog brisanja, inace false.</returns>
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Blok2Projekat/WCFCommon/IServiceComms.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum ModifyType { Edit, Delete };
+
+namespace WCFCommon
+{
+    //Format poruke za upis/modifikaciju koja se salje sa klijenta: Timestamp:X;Details:X;
+    [ServiceContract]
+    public interface IServiceComms
+    {
+        /// <summary>
+        /// Read koji se poziva iz klijenta. SID se NE prosledjuje; utvrdjuje se na strani servisa.
+        /// </summary>
+        /// <returns>String koji sadrzi svaki red vezan za datog klijenta.</returns>
+        [OperationContract]
+        string Read();
+        /// <summary>
+        /// Zahtev za modifikaciju baze podataka sa klijentske strane.
+        /// </summary>
+        /// <param name="type">Tip modifikacije.</param>
+        /// <param name="id">ID podatka koji treba modifikovati.</param>
+        /// <param name="newVersion">Zeljeni novi sadrzaj tog podatka. Formatiranje se radi na strani klijenta.</param>
+        /// <returns></returns>
+        [OperationContract]
+        bool Modify(ModifyType type, string id, string newVersion);
+        /// <summary>
+        /// Zahtev za prijavu na promene baze podataka. Ispisuje svaku novu izmenu na bazi podataka na konzolu posle uspesne konekcije.
+        /// </summary>
+        /// <returns>Vraca true u slucaju uspesnog zahteva, inace false. Ako se petlja ispisa promena na bazi podataka implementira kao zasebna nit, ne zaboravi da implementiras
+        /// graceful shutdown.</returns>
+        [OperationContract]
+        bool Subscribe();
+        /// <summary>
+        /// Slanje dogadjaja na servis. U realnom sistemu, ovo bi bila neka druga funkcija koja u svom okviru generise dogadjaje, ali ovde je
+        /// predvidjeno da se salje jedan Event() na svaku sekundu, generisan iz resursnog fajla.
+        /// </summary>
+        /// <param name="generatedEvent">Nasumice generisan Event() iz resursnog fajla</param>
+        /// <returns>Vraca true ako je dogadjaj uspesno upisan, inace false.</returns>
+        [OperationContract]
+        bool Event(string generatedEvent);
+    }
+}
